f491d695dfa7418ca5eedd73f1cfcbae054ecb40
Rouel <astrolabio1417@gmail.com>
2022-07-28 13:11:19 +0800

    fix error

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,6 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.home),
     path("admin/", admin.site.urls),
-    path("projects/", include("project.urls")),
     path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
