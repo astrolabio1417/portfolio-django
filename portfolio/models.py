e34c8b10e961504e2feb9255e2ed53ddbc9afd1a
Rouel <astrolabio1417@gmail.com>
2022-10-09 21:17:57 +0800

    change tech choices and page style

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
-TECHNOLOGY_CHOICES = [
-    ("L", "LANGUAGE"),
-    ("F", "FRAMEWORK"),
-    ("P", "PLATFORM"),
-]
 
 
 class Technology(models.Model):
+    TECHNOLOGY_CHOICES = [
+        ("L", "LANGUAGE"),
+        ("F", "FRAMEWORK"),
+        ("BE", "BACKEND"),
+        ("FE", "FRONTEND"),
+    ]
     name = models.CharField(max_length=25)
     logo = models.FileField(upload_to="logo")
-    type = models.CharField(choices=TECHNOLOGY_CHOICES, max_length=1)
+    type = models.CharField(choices=TECHNOLOGY_CHOICES, max_length=2)
 
     def __str__(self) -> str:
         return f"{self.name} | {self.type}"
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,8 +6,9 @@ from .models import Technology, Project
 def home(request):
     technologies = Technology.objects.all()
     languages = technologies.filter(type="L")
-    frameworks = technologies.filter(type="F")
     platforms = technologies.filter(type="P")
+    frontends = technologies.filter(type="FE")
+    backends = technologies.filter(type="BE")
     projects = Project.objects.all()
     media_url = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}"
 
@@ -19,9 +20,10 @@ def home(request):
         "home.html",
         {
             "technologies": {
-                "languages": languages,
-                "frameworks": frameworks,
-                "platforms": platforms,
+                "language": languages,
+                "frontend": frontends,
+                "backend": backends,
+                "platform": platforms,
             },
             "projects": projects,
             "media_url": media_url,
