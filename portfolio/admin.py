5e68c5ad48cb79d80e27d8673dcea690bcee599e
astrolabio1417 <astrolabio1417@gmail.com>
2024-01-23 02:36:04 +0800

    feat: users form on admin page

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from portfolio.models import Education, Experience, Project, Technology, User
 
-admin.site.register(User)
+from django.contrib.auth.admin import UserAdmin
+
+
+UserAdmin.fieldsets[1][1]["fields"] = (
+    "github_url",
+    "contact_number",
+    "resume",
+    "linked_url",
+    "about",
+    "technologies",
+)
+
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (*UserAdmin.fieldsets,)
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Education)
 admin.site.register(Experience)
 admin.site.register(Project)
