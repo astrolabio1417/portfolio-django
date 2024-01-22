42ac7c91976ad914bc49175b23e287667ceb6d76
astrolabio1417 <astrolabio1417@gmail.com>
2024-01-23 02:29:14 +0800

    fix: remove temp fixture

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py loaddata fixtures/initdata.json
 
 exec gunicorn portfolio.wsgi --bind 0.0.0.0:$PORT --workers 4 --thread 4 --timeout 300 --access-logfile '-' --error-logfile '-'
