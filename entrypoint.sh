051307074e1d3a4018c1cf82cfabe2870f2b71b7
astrolabio1417 <astrolabio1417@gmail.com>
2024-01-23 02:26:17 +0800

    fix: remove waiting for db, temporary entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-while ! ./manage.py sqlflush > /dev/null 2>&1 ;do
-    echo "Waiting for the db to be ready."
-    sleep 1
-done
-
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py loaddata fixtures/initdata.json
