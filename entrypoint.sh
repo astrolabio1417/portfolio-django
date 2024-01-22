c4b6f52e1ad4284fb08fb9747ea171b8590e75e8
astrolabio1417 <astrolabio1417@gmail.com>
2024-01-23 02:23:48 +0800

    feat: starting fixtures, temporary entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+while ! ./manage.py sqlflush > /dev/null 2>&1 ;do
+    echo "Waiting for the db to be ready."
+    sleep 1
+done
+
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py loaddata fixtures/initdata.json
 
 exec gunicorn portfolio.wsgi --bind 0.0.0.0:$PORT --workers 4 --thread 4 --timeout 300 --access-logfile '-' --error-logfile '-'
